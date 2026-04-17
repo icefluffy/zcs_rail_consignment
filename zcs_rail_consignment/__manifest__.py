aad3bff51e22b31e2335432391397bfacaef76e0
icefluffy <icewitty@gmail.com>
2026-04-17 17:06:52 +0200

    g

--- zcs_rail_consignment/__manifest__.py
+++ zcs_rail_consignment/__manifest__.py
@@ -6,8 +6,8 @@
     "depends": ["base"],
     "data": [
         "security/ir.model.access.csv",
-        "data/rail_company_codes.csv",  # From PDF extracted CSV coming from uic.org
-        "data/rail_company_data.xml",   # NEW to structure CSV data
+        #"data/rail_company_codes.csv",  # From PDF extracted CSV coming from uic.org
+        #"data/rail_company_data.xml",   # NEW to structure CSV data
         "views/rail_consignment_views.xml",
     ],
     "installable": True,
