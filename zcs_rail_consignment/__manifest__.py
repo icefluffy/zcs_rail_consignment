a16f0dae5a2d631464f4ec23ba6ac8a8690add4f
icefluffy <icewitty@gmail.com>
2026-04-18 17:54:35 +0200

    order of models in init.py

--- zcs_rail_consignment/__manifest__.py
+++ zcs_rail_consignment/__manifest__.py
@@ -4,10 +4,10 @@
     "category": "Inventory/Shipments",
     "summary": "Validate rail vehicles and containers identification numbers",
     "depends": ["base"],
-    "data": [
-        "data/rail_company_code.csv",  # From PDF extracted CSV coming from uic.org        
+    "data": [        
         "security/ir.model.access.csv",
         "views/rail_consignment_views.xml",
+        "data/rail_company_code.csv",  # From PDF extracted CSV coming from uic.org
     ],
     "installable": True,
     "application": False,
