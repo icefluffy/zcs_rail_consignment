78927c6f2615470e13eb51138ef8271bde6b9ea7
icefluffy <icefluffy@users.noreply.github.com>
2026-04-20 16:39:36 +0200

    Change application status to True in manifest

--- zcs_rail_consignment/__manifest__.py
+++ zcs_rail_consignment/__manifest__.py
@@ -4,11 +4,11 @@
     "category": "Inventory/Shipments",
     "summary": "Validate rail vehicles and containers identification numbers",
     "depends": ["base"],
-    "data": [        
+    "data": [
         "security/ir.model.access.csv",
         "views/menu.xml",
     ],
     "post_init_hook": "post_init_load_rail_company_codes",
     "installable": True,
-    "application": False,
+    "application": True,
 }
