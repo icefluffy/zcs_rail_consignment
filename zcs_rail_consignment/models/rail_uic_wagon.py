ef782a99880e8a9af3f14fa66d6dcbff4b7395c7
icefluffy <icewitty@gmail.com>
2026-04-22 17:24:07 +0200

    views

--- zcs_rail_consignment/models/rail_uic_wagon.py
+++ zcs_rail_consignment/models/rail_uic_wagon.py
@@ -6,5 +6,5 @@ class RailUICWagon(models.Model):
     _description = "UIC Wagon Code Decoder"
     _inherit = ["rail.wagon.mixin"]
 
-    name = fields.Char(string="Reference", required=True, default="New")
+    name = fields.Char(string="Reference", default="New")
     notes = fields.Text(string="Notes")
